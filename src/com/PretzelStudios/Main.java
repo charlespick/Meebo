eeadd2db30baee5e47f3016a630770197db499c3
Charles <17918019+charlespick@users.noreply.github.com>
2018-09-18 09:31:22 -0700

    For loops and cleanup

--- src/com/PretzelStudios/Main.java
+++ src/com/PretzelStudios/Main.java
@@ -4,14 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Alpha positions
         char[] rawString = "Hello".toUpperCase().toCharArray();
         for (int i = 0; i < rawString.length; i++) {
             System.out.println((int) rawString[i] - 65);
         }
+
+        //Ceasar shift
         for (int i = 0; i < rawString.length; i++) {
             System.out.println((char) (rawString[i] + 3));
         }
 
+        //Shopping cart
         double iPhoneXSMaxPrice = 1449.00;
         double appleCarePlus = 199.00;
         double xscase = 39.00;
@@ -23,6 +27,39 @@ public class Main {
         subTotal += 7.95;
         System.out.println(subTotal);
 
+        //A true fact
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("All work and no play makes Jack a dull boy");
+        }
+
+        //Prints numbers
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+
+        //Prints sum of numbers 1 - 1000
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) {
+            count += i;
+        }
+        System.out.println(count);
+
+        //120 factorial
+        int factorial = 1;
+        for (int i = 5; i > 0; i--) {
+            factorial *= i;
+        }
+        System.out.println(factorial);
+
+        //Multiplication table
+        System.out.println("  1 2 3 4 5");
+        for (int a = 1; a <= 5; a++) {
+            System.out.print(a + ":");
+            for (int b = 1; b <= 5; b++) {
+                System.out.print(a * b + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
