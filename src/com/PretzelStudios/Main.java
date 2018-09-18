aa0d8accb5e4ee927329a5be2ebd12c92e54971d
Charles <17918019+charlespick@users.noreply.github.com>
2018-09-17 20:40:57 -0700

    How much does an iPhone XS really cost

--- src/com/PretzelStudios/Main.java
+++ src/com/PretzelStudios/Main.java
@@ -5,8 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
         char[] rawString = "Hello".toUpperCase().toCharArray();
-        for (int i = 0; i < rawString.length; i++) { System.out.println((int) rawString[i] - 65); }
-        for (int i = 0; i < rawString.length; i++) { System.out.println((char) (rawString[i] + 3)); }
+        for (int i = 0; i < rawString.length; i++) {
+            System.out.println((int) rawString[i] - 65);
+        }
+        for (int i = 0; i < rawString.length; i++) {
+            System.out.println((char) (rawString[i] + 3));
+        }
+
+        double iPhoneXSMaxPrice = 1449.00;
+        double appleCarePlus = 199.00;
+        double xscase = 39.00;
+        double powerAdapter = 19.00;
+        double adapter = 49.00;
+
+        double subTotal = iPhoneXSMaxPrice + appleCarePlus + xscase + powerAdapter + adapter;
+        subTotal *= 1.1;
+        subTotal += 7.95;
+        System.out.println(subTotal);
 
 
     }
