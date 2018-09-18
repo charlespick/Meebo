6dbca0cc1f23b725aa1fb6365a901a0901ad657a
Charles <17918019+charlespick@users.noreply.github.com>
2018-09-17 17:42:10 -0700

    Closing Monday

--- src/com/PretzelStudios/Main.java
+++ src/com/PretzelStudios/Main.java
@@ -4,24 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String targetString = "Hello";
-        char[] rawString = targetString.toUpperCase().toCharArray();
-
-        for (int i = 0; i<targetString.length(); i++) {
-            System.out.println((int)rawString[i]-65);
-        }
-
-        System.out.println();
-
-        for (int i = 0; i<targetString.length(); i++){
-            System.out.println((char)(rawString[i]+3));
-        }
-
-
-
-
-
-
+        char[] rawString = "Hello".toUpperCase().toCharArray();
+        for (int i = 0; i < rawString.length; i++) { System.out.println((int) rawString[i] - 65); }
+        for (int i = 0; i < rawString.length; i++) { System.out.println((char) (rawString[i] + 3)); }
 
 
     }
